7a7d7ded798347ae9fb4c88d41c981af56ed81fa
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-09-28 17:21:44 +1000

    Update AutoType.py
    Added random to interval to vary the text typing speed

--- AutoType.py
+++ AutoType.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 
 # path of donor file to read from
 donorFile = 'E:/framework2232/Python/donor.html'
@@ -7,11 +8,12 @@ donorFile = 'E:/framework2232/Python/donor.html'
 # time to setup (in seconds) before typing begins
 time.sleep(5)
 
+value=0.2
+
 with open(donorFile, 'r') as openFile:
     
-    for line in openFile:     
-
-        pyautogui.typewrite(line, interval=0.08) 
+    for line in openFile:   
+        pyautogui.typewrite(line, interval= (random.uniform(0.05, 0.15))) 
 
         i = 0
         while i < 11:
@@ -20,6 +22,9 @@ with open(donorFile, 'r') as openFile:
         
         time.sleep(0.05)
         pyautogui.hotkey("shift", "alt", "f") 
-        time.sleep(0.5)
+        time.sleep(0.05)
         pyautogui.typewrite(['enter']) 
+        time.sleep(0.5)
+        # value = random.uniform(0.08, 0.4)
+        # print(value)
         
