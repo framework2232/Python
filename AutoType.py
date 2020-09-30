cfec7a1dff010956e767d5707c4fd0f3fcbae1da
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-09-30 21:21:58 +1000

    autotype
    updated the Auto Type file in pilot development ATM

--- AutoType.py
+++ AutoType.py
@@ -2,18 +2,18 @@ import pyautogui
 import time
 import random
 
-# path of donor file to read from
-donorFile = 'E:/framework2232/Python/donor.html'
-
 # time to setup (in seconds) before typing begins
-time.sleep(5)
+time.sleep(10)
 
-value=0.2
+
+
+# path of donor file to read from
+donorFile = 'E:/framework2232/Python/donor_html.html'
 
 with open(donorFile, 'r') as openFile:
     
     for line in openFile:   
-        pyautogui.typewrite(line, interval= (random.uniform(0.05, 0.15))) 
+        pyautogui.typewrite(line, interval= 0.08) 
 
         i = 0
         while i < 11:
@@ -25,6 +25,21 @@ with open(donorFile, 'r') as openFile:
         time.sleep(0.05)
         pyautogui.typewrite(['enter']) 
         time.sleep(0.5)
-        # value = random.uniform(0.08, 0.4)
-        # print(value)
         
+
+time.sleep(10)
+# path of donor file to read from
+donorFile = 'E:/framework2232/Python/donor_css.css'
+
+with open(donorFile, 'r') as openFile:
+    
+    for line in openFile:   
+        pyautogui.typewrite(line, interval= 0.08)
+
+        i = 0
+        while i < 11:
+            i = i + 1
+            pyautogui.typewrite(['del'])
+        
+        time.sleep(0.05)
+       
